c41428e18e83d521a9fd284fd3b1bd6cb18f96a3
xodn_ghkd <hwa1023@mjc.ac.kr>
2022-08-25 22:13:09 +0900

    json

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import Contents from "./Contents-json";
+import styled from "./App.module.css";
+import { useEffect, useState } from "react";
 
-function App() {
+const App = () => {
+  // const [list, setList] = useState([]);
+  const [input, setInput] = useState("");;
+
+  const Visable = (input) => {
+    Contents.map((item, index) => (
+      item.tags.map((tag) => (
+        input.toUpperCase() === tag.toUpperCase()
+          ? Contents[index].select = true
+          : Contents.map((item2, index) => (
+            item === item2 ? null : Contents[index].select = false
+          ))
+      ))
+    ))
+  }
+
+  const onChange = (e) => {
+    setInput(e.target.value);
+    Visable(e.target.value);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setInput("");
+  }
+
+  useEffect(() => {
+    // setList(Contents);
+  }, []);
+  // console.log(list);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={input} className={styled.input} />
+        <button>Click</button>
+        <div className={styled.Div}>
+          {Contents.map((data, index) => (
+            <div key={index} className={styled.ContentsDiv} id={data.select ? null : "hide"}>
+              <img src={data.Imgurl} alt={`${data.title} 이미지`} />
+              <h1>{data.title}</h1>
+              <p>{data.description}</p>
+              <ul>
+                {data.tags.map((tag, index) => (
+                  <li key={index}>#{tag}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </form>
+    </>
   );
 }
 

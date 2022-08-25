ee8d7e468df9ace087ceaf1882f59a5184c69efc
root <root@DESKTOP-NLGT05I.localdomain>
2022-08-25 23:25:42 +0900

    search filter

--- src/App.js
+++ src/App.js
@@ -3,55 +3,68 @@ import styled from "./App.module.css";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  // const [list, setList] = useState([]);
+  const [list, setList] = useState([]);
   const [input, setInput] = useState("");;
 
-  const Visable = (input) => {
-    Contents.map((item, index) => (
-      item.tags.map((tag) => (
-        input.toUpperCase() === tag.toUpperCase()
-          ? Contents[index].select = true
-          : Contents.map((item2, index) => (
-            item === item2 ? null : Contents[index].select = false
-          ))
-      ))
-    ))
-  }
+  // const onChange = (e) => {
+  //   setInput(e.target.value);
+  // }
 
-  const onChange = (e) => {
-    setInput(e.target.value);
-    Visable(e.target.value);
-  }
-
-  const onSubmit = (e) => {
-    e.preventDefault();
-    setInput("");
-  }
+  // const onSubmit = (e) => {
+  //   e.preventDefault();
+  //   setInput("");
+  // }
 
   useEffect(() => {
-    // setList(Contents);
+    setList(Contents);
   }, []);
   // console.log(list);
+  const mapToComponents = (datas) => {
+    datas.sort();
+    datas.map((data) => (
+      data.tags.map((tag) => (
+        tag = tag.toUpperCase()
+      ))
+    ))
+    datas = datas.filter((data) => {
+      if (input === "") {
+        return data;
+      }
+      else if (data.tags.indexOf(input) > -1) {
+        return true;
+      }
+      return null;
+    });
+    return datas.map((data, index) => (
+      <div key={index} className={styled.ContentsDiv}>
+        <img src={data.Imgurl} alt={`${data.title} 이미지`} />
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
+        <ul>
+          {data.tags.map((tag, index) => (
+            <li key={index}>#{tag}</li>
+          ))}
+        </ul>
+      </div>
+    ))
+  }
+  const onClick = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value);
+    console.log(input);
+  }
   return (
     <>
-      <form onSubmit={onSubmit}>
-        <input onChange={onChange} value={input} className={styled.input} />
-        <button>Click</button>
-        <div className={styled.Div}>
-          {Contents.map((data, index) => (
-            <div key={index} className={styled.ContentsDiv} id={data.select ? null : "hide"}>
-              <img src={data.Imgurl} alt={`${data.title} 이미지`} />
-              <h1>{data.title}</h1>
-              <p>{data.description}</p>
-              <ul>
-                {data.tags.map((tag, index) => (
-                  <li key={index}>#{tag}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      </form>
+      {/* <input onChange={onChange} value={input} className={styled.input} /> */}
+      {/* <button onClick={onClick} value="TS">#TS</button> */}
+      {list.map((data) => (
+        data.tags.map((tag) => (
+          <button onClick={onClick} value={tag}>#{tag}</button>
+        ))
+      ))}
+      <div className={styled.Div}>
+        {mapToComponents(list)}
+      </div>
     </>
   );
 }
